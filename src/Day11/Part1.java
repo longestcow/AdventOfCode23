199df098a66f368ef8ed7b9f54463d4fcb1ce43b
mambo <omiyer76@gmail.com>
2023-12-11 01:25:29 -0600

    day 11 foundation

--- src/Day11/Part1.java
+++ src/Day11/Part1.java
@@ -10,22 +10,21 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Part1 {
-	static int size = 140;
 
-	static char[][] grid = new char[size][];
-	static String curr1="", curr2="";
-	static HashMap<String,Integer> visited = new HashMap<>();
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		List<String> lines = new ArrayList<>();
+		List<Integer> cols = new ArrayList<>(), rows = new ArrayList<>();
+
 		try (BufferedReader br = new BufferedReader(new FileReader(new File("src/Day11/input.txt")))) {
 			String line;
+			int i = 0;
 		    while ((line = br.readLine()) != null) {
 		    	lines.add(line);
-		    	if(!line.contains("#"))lines.add(line);
+		    	if(!line.contains("#"))rows.add(i);
+		    	i++;
 		    }
 	    }
 		boolean s;
-		List<Integer> cols = new ArrayList<>();
 		for(int i = 1; i<lines.get(0).length()-1; i++) {
 			s=true;
 			for(String line : lines) {
@@ -36,16 +35,13 @@ public class Part1 {
 			}
 			if(s)cols.add(i);
 		}
-		int c = 0;
-		for(int i : cols) {
-			for(int j = 0; j<lines.size(); j++) 
-				lines.set(j, lines.get(j).substring(0,i+c)+"."+lines.get(j).substring(i+c));
-			c++;
-		}
+
+
 		
 		char[][] grid = new char[lines.size()][lines.get(0).length()];
 		for(int i = 0; i<lines.size(); i++)grid[i]=lines.get(i).toCharArray();
-		
+		System.out.println(rows);
+		System.out.println(cols);
 		//pathfind code begins here
 		
 	}
