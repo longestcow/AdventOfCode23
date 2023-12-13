72a9b1cb4ce953432354bee68d0b904e3406a0e1
mambo <omiyer76@gmail.com>
2023-12-12 21:42:39 -0600

    day 12 stuff

--- src/Day12/Part1.java
+++ src/Day12/Part1.java
@@ -24,24 +24,31 @@ public class Part1 {
 		    	map.put(line.split(" ")[0], currI);
 		    }
 	    }
-		int sum = 0;
+		int sum = 0, curr=0;
 		for(String key : map.keySet()) {
+			curr=0;
 			List<Integer> qs = new ArrayList<Integer>();
 			for(int i = 0; i<key.length(); i++)
 				if(key.charAt(i)=='?')qs.add(i);
-			sum+=dfs("#", key, map.get(key), qs, 0);
-			sum+=dfs(".", key, map.get(key), qs, 0);
+			curr+=dfs("#", key, map.get(key), qs, 0);
+			curr+=dfs(".", key, map.get(key), qs, 0);
+			sum+=curr;
 		}
 		System.out.println(sum);
 	}
 	
 	static int dfs(String pick, String s, List<Integer> i, List<Integer> qs, int ind) {
-		if(ind==qs.size()-1)
-			return checkValid(s,i,qs.get(ind))?1:0;
 		s=s.substring(0,qs.get(ind))+pick+s.substring(qs.get(ind)+1);
-		int sum = 0;
+		if(ind==qs.size()-1) {
+			if(checkValid(s,i,s.length()-1))
+				return 1;
+			else return 0;
+		}
+		
 		if(!checkValid(s,i,qs.get(ind)))
 			return 0;
+		
+		int sum = 0;
 
 		sum+=dfs("#", s, i, qs, ind+1);
 		sum+=dfs(".", s, i, qs, ind+1);
@@ -49,21 +56,22 @@ public class Part1 {
 	}
 	
 	static boolean checkValid(String s, List<Integer> i, int length) {
-		s=s.substring(0, length+1);
-		if(!s.contains("?")) {
-			int c = 0;
-			for(String a : s.split("\\.")) {
-				if(a.isEmpty())continue;
-				try {
-					if(a.length()!=i.get(c))
-						return false;
-				}catch(IndexOutOfBoundsException e) {return false;}
+		s=s.substring(0, length);
+		int c = 0;
+		for(String a : s.split("\\.")) {
+			if(a.isEmpty())continue;
+			try {
+				if(a.length()!=i.get(c))
+					return false;
 				c++;
-			}
-			if(i.size()==c+1);
-			return true;
+
+			}catch(IndexOutOfBoundsException e) {return false;}
 		}
+		if(i.size()==c)
+			return true;
 		return false;
+	
+
 	}
 	
 
